5032a8642d50f18a68845e9d271f46d0ce114c78
MichaelNeal572 <michaelneal9675@gmail.com>
2019-10-16 13:41:22 +0200

    Basic Logger(Output=".log file") + initial .gitignore

--- fault source.py
+++ fault source.py
@@ -1,4 +1,16 @@
-def sampleMath:
+from functools import wraps
+
+def my_logger(func):
+	import logging
+	logging.basicConfig(filename='{}.log'.format(func.__name__), level=logging.INFO)
+
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		logging.info('Ran with args: {}, and kwargs {}'.format(args, kwargs))
+	return wrapper
+
+@my_logger
+def sampleMath():
 		i=5/0
 		return i
 
