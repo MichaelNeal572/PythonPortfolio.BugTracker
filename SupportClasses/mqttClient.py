a91f3ae0cb28ecf80b2bbfecc09df652747c9fcf
MichaelNeal572 <michaelneal9675@gmail.com>
2019-10-21 18:07:21 +0200

    Finished basic server side database setup... mqttClient not getting response

--- SupportClasses/mqttClient.py
+++ SupportClasses/mqttClient.py
@@ -7,44 +7,52 @@ class mqttClient():
         self.port = 1883
         self.user = "pi"
         self.password = "mike"
-        self.Connected=False
+        self.connected=False
+        self.response=False
+        self.sub_topic="python/bugReport/Response"
+        self.pub_topic="python/bugReport/BugReportsCRUD"
 
         self.client = mqttClientInterface.Client("Python")               #create new instance
         self.client.username_pw_set(self.user, password=self.password)    #set username and password
-        print(self.on_connect)
         self.client.on_connect= self.on_connect                 #attach function to callback
-        
+        self.client.on_message=self.on_message
+        self.client.on_log=self.on_log
+    
+    def on_log(client, userdata, level, buf):
+        print("log: ",buf)
+
+    def on_message(client, userdata, message):
+        print(f"message payload: {message.payload}")
+        print(f"massage topic: {message.topic}")
+        self.response=True
  
     def on_connect(self, client, userdata, flags, rc):
-     
         if rc == 0:
             print("Connected to broker")
-            self.Connected = True                #Signal connection 
+            self.connected = True                #Signal connection 
         else:
             print("Connection failed")
-            self.Connected = False   #global variable for the state of the connection
+            self.connected = False   #global variable for the state of the connection
 
-    def publish(self, topic, payload):
+    def send(self, payload):
         self.client.connect(self.broker_address, port=self.port)          #connect to broker
         self.client.loop_start()
-        while self.Connected!=True:
+        self.client.subscribe(self.sub_topic)
+
+        while (not self.connected):
             time.sleep(0.1)
-        self.client.publish(topic, payload)
-        self.client.disconnect()
+        self.client.publish(self.pub_topic, payload)
+        
+        
+        while (not self.response):
+            time.sleep(0.1)
+            print("waiting")
+
+        print("Complete")
+        self.client.loop_stop()
  
 
 if __name__=="__main__":
     ##TESTING LINES
     cl = mqttClient()
-    cl.client.loop_start()        #start the loop
-     
-    while cl.Connected != True:    #Wait for connection
-        print("Connected: "+str(cl.Connected))
-        time.sleep(0.1)
-    try:
-        while True:
-            value = input('Enter the message:')
-            cl.client.publish("python/test",value)
-    except KeyboardInterrupt:
-        cl.client.disconnect()
-    ##
+    cl.send("Test")
