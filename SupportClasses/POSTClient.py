d9f4debf0e01f3abc4a0be97ef3fbdf11437f396
MichaelNeal572 <michaelneal9675@gmail.com>
2019-10-26 14:47:08 +0200

    POST request mock inserts successful

--- SupportClasses/POSTClient.py
+++ SupportClasses/POSTClient.py
@@ -1,14 +1,33 @@
 import requests
+from datetime import datetime
 import time
 
 class POSTClient():
-    
-    def send(self, payload):
-        self.r = requests.post("http://192.168.8.201:1880/InsertBugRecord", data={'number': 1234, 'type':'issue', 'action':'show'})
-        print(self.r.status_code, self.r.reason)
-        print(self.r.text)
+    def insert_bug(self, bugDetails, bugArguments, bugSource,bugDateCreated, bugStatus, bugExpectedDevTime):
+        url = "http://192.168.8.201:1880/InsertBugRecord"
+        message={
+            "bugDetails":bugDetails, 
+            "bugArguments":bugArguments, 
+            "bugSource":bugSource,
+            "bugDateCreated":bugDateCreated, 
+            "bugStatus":bugStatus, 
+            "bugExpectedDevTime":bugExpectedDevTime
+        }
+        return(self.send(message, url))
+
+    def insert_bug_new(self, bugDetails, bugArguements, bugSource):
+        return(self.insert_bug(bugDetails, bugArguements, bugSource, datetime.now(), "New", "TBD"))
+
+    def send(self, message, url):
+        r = requests.post(url, data=message)
+        response={
+            "status_code":r.status_code,
+            "reason":r.reason,
+            "text":r.text
+        }
+        return response
 
 if __name__ == "__main__":
     ##TESTING LINES
     cl = POSTClient()
-    print(cl.send('test'))
+    print(cl.insert_bug_new('Test', 'Test', 'Test'))
--- SupportClasses/faultLogger.py
+++ SupportClasses/faultLogger.py
@@ -1,5 +1,6 @@
 from functools import wraps
 
+
 def my_logger(func):
 	import logging
 	logging.basicConfig(filename='{}.log'.format(func.__name__), level=logging.INFO)
