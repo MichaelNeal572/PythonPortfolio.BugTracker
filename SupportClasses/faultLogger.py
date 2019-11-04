e0552c9a1a87567e451366a9f8dd8b6cf5b1061c
MichaelNeal572 <michaelneal9675@gmail.com>
2019-11-04 13:57:33 +0200

    error details are now captured correctly by the logger

--- SupportClasses/faultLogger.py
+++ SupportClasses/faultLogger.py
@@ -1,10 +1,19 @@
 from functools import wraps
+from SupportClasses import POSTClient
+import datetime
+
+pc = POSTClient.POSTClient()
+
+def insert_bug_record(details, args, kwargs, source, dateCreated, status, expectedResolution):
+        message=f'''INSERT INTO bugs (bugDetails, bugArguments, bugSource, bugDateCreated, bugStatus, bugExpectedResolution)
+        VALUES ("{details}", "args: {args} kwargs: {kwargs}", "{source}", "{dateCreated}", "{status}", "{expectedResolution}")
+        '''
+        return(pc.send(message))
 
 
 def my_logger(func):
 	import logging
 	logging.basicConfig(filename='{}.log'.format(func.__name__), level=logging.INFO)
-
 	@wraps(func)
 	def wrapper(*args, **kwargs):
 		info_message='Ran with args: {}, and kwargs {}'.format(args, kwargs)
@@ -15,6 +24,13 @@ def my_logger(func):
 		except Exception as e:
 			logging.info(e)
 			logging.info("#####################################")
+			
+			details = e
+			source = func.__name__
+			dateCreated = datetime.datetime.now()
+			status = "NEW"
+			expectedResolution = "TBD"
+			insert_bug_record(details, args, kwargs, source, dateCreated, status, expectedResolution)
 			return("Exception Thrown")
 
 	return wrapper
